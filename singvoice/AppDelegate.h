6afae3dab9ef6b45aa45819e852c96bc78fe9c8b
usertoandy <wildusk@gmail.com>
2016-12-16 12:41:00 +0800

    init

---
+++ singvoice/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  singvoice
+//
+//  Created by Andy on 15/12/2016.
+//  Copyright © 2016 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
